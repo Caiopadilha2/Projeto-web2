cda92d74da365543c528366f0388e0a8cedad692
Caio Padilha <caiopadilha@id.uff.br>
2025-03-04 18:09:58 -0300

    Escolher gabarito

--- scripts.js
+++ scripts.js
@@ -1,83 +1,108 @@
-const options = document.querySelectorAll('.options');
-const reset = document.querySelector('.reset');
-const count = document.querySelector('.count');
-const pontos = document.querySelector('.pontuacao');
-const screen = document.querySelector('.screen');
+const options = document.querySelectorAll('.options')
+const reset = document.querySelector('.reset')
+const count = document.querySelector('.count')
+const pontos = document.querySelector('.pontuacao')
+const screen = document.querySelector('.question')
+const getInput = document.querySelector('.cod')
+const btnIniciar = document.querySelector('.btn-submit')
 
-const gabarito1 = ['A', 'B', 'C', 'D', 'A', 'B', 'C', 'D'];
-const gabarito2 = ['A', 'B', 'C', 'C', 'D', 'A', 'B', 'C'];
-const gabarito3 = ['A', 'B', 'C', 'A', 'B', 'C', 'D', 'A'];
-const gabarito4 = ['A', 'B', 'C', 'D', 'D', 'B', 'B', 'C'];
+let gabarito
+btnIniciar.addEventListener('click', event => {
+  // Ver se da para tirar o evento de submit
+  event.preventDefault()
+  let codInput = getInput.value
 
-let index = 0;
-let tentativas = 0;
-let pontuacao = 0; 
+  switch (codInput) {
+    case '101':
+      gabarito = gabarito1
+      // Adicionar musica
+      break
+    case '102':
+      gabarito = gabarito2
+      // Adicionar musica
+      break
+    case '103':
+      gabarito = gabarito3
+      // Adicionar musica
+      break
+    case '104':
+      gabarito = gabarito4
+      // Adicionar musica
+      break
+  }
+})
 
+const gabarito1 = ['A', 'B', 'C', 'D', 'A', 'B', 'C', 'D']
+const gabarito2 = ['D', 'B', 'C', 'C', 'D', 'A', 'B', 'C']
+const gabarito3 = ['A', 'B', 'C', 'A', 'B', 'C', 'D', 'A']
+const gabarito4 = ['A', 'B', 'C', 'D', 'D', 'B', 'B', 'C']
 
-count.innerText = `${tentativas + 1} de 3`;
-pontos.innerText = pontuacao;
+let index = 0
+let tentativas = 0
+let pontuacao = 0
+
+count.innerText = `${tentativas + 1} de 3`
+pontos.innerText = pontuacao
 
 if (index <= 8) {
-    screen.innerText = index + 1;
+  screen.innerText = index + 1
 }
 
+options.forEach(option => {
+  option.addEventListener('click', event => {
+    if (index >= gabarito.length) {
+      alert('O quiz já terminou!')
+      return
+    }
 
-options.forEach((option) => {
-    option.addEventListener('click', (event) => {
-        if (index >= gabarito1.length) {
-            alert("O quiz já terminou!");
-            return;
-        }
+    if (gabarito[index] === event.currentTarget.innerText) {
+      console.log(`Acertou na tentativa ${tentativas + 1}!`)
 
-        if (gabarito1[index] === event.currentTarget.innerText) {
-            console.log(`Acertou na tentativa ${tentativas + 1}!`);
-            
-            switch (tentativas) {
-                case 0:
-                    pontuacao += 3;
-                    break;
-                case 1:
-                    pontuacao += 2;
-                    break;
-                case 2:
-                    pontuacao += 1;
-                    break;
-            }
+      switch (tentativas) {
+        case 0:
+          pontuacao += 3
+          break
+        case 1:
+          pontuacao += 2
+          break
+        case 2:
+          pontuacao += 1
+          break
+      }
 
-            pontos.innerText = pontuacao;
-            tentativas = 0;
-            index += 1;
-        } else {
-            console.log(`Errou! tentativa: ${tentativas + 1}!`);
-            tentativas += 1;
+      pontos.innerText = pontuacao
+      tentativas = 0
+      index += 1
+    } else {
+      console.log(`Errou! tentativa: ${tentativas + 1}!`)
+      tentativas += 1
 
-            if (tentativas === 3) {
-                tentativas = 0;
-                index += 1;
-            }
-        }
+      if (tentativas === 3) {
+        tentativas = 0
+        index += 1
+      }
+    }
 
-        count.innerText = `${tentativas + 1} de 3`;
+    count.innerText = `${tentativas + 1} de 3`
 
-        if (index < 8) {
-            screen.innerText = index + 1;
+    if (index < 8) {
+      screen.innerText = index + 1
+    }
 
-        }
+    if (index >= gabarito1.length) {
+      alert(`Quiz finalizado!`)
+    }
 
-
-        if (index >= gabarito1.length) {
-            alert(`Quiz finalizado!`);
-        }
-        
-        console.log(`Pontuação: ${pontuacao}`);
-    });
-});
+    console.log(`Pontuação: ${pontuacao}`)
+  })
+})
 
 reset.addEventListener('click', () => {
-    index = 0;
-    tentativas = 0;
-    pontuacao = 0;
-    count.innerText = `${tentativas + 1} de 3`;
-    pontos.innerText = pontuacao;
-    alert("Quiz reiniciado!");
-});
+  index = 0
+  tentativas = 0
+  pontuacao = 0
+  count.innerText = `${tentativas + 1} de 3`
+  pontos.innerText = pontuacao
+  screen.innerText = 1
+  alert('Quiz reiniciado!')
+})
