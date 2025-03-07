f22a5a579d91e2bbc9c45631a0cbbc04bb5e271e
Caio Padilha <caiopadilha@id.uff.br>
2025-03-07 17:20:28 -0300

    fotos

--- scripts.js
+++ scripts.js
@@ -6,14 +6,47 @@ const screen = document.querySelector('.question')
 const getInput = document.querySelector('.cod')
 const btnIniciar = document.querySelector('.btn-submit')
 const imagem = document.querySelector('.imagem')
+const teclado = document.querySelectorAll('.teclado')
+
+const gabarito1 = ['B', 'B', 'C', 'D', 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D', 'A', 'B']
+const gabarito2 = ['D', 'B', 'C', 'C', 'D', 'A', 'B', 'C']
+const gabarito3 = ['A', 'B', 'C', 'A', 'B', 'C', 'D', 'A']
+const gabarito4 = ['A', 'B', 'C', 'D', 'D', 'B', 'B', 'C']
 
 const images1 = [
   './assets/Question-01.png',
   './assets/Question-02.png',
-  './assets/Question-03.png'
+  './assets/Question-03.png',
+  './assets/Question-04.png',
+  './assets/Question-05.png',
+  './assets/Question-06.png',
+  './assets/Question-07.png',
+  './assets/Question-08.png',
+  './assets/Question-09.png',
+  './assets/Question-10.png',
+  './assets/Question-11.png',
+  './assets/Question-12.png',
+  './assets/Question-13.png',
+  './assets/Question-14.png',
+  './assets/Question-15.png',
+  './assets/Question-16.png',
+  './assets/Question-17.png',
+  './assets/Question-18.png',
+  './assets/Question-19.png',
+  './assets/Question-20.png',
+  './assets/Question-21.png',
+  './assets/Question-22.png',
+  './assets/Question-23.png',
+  './assets/Question-24.png',
+  './assets/Question-25.png',
+  './assets/Question-26.png',
+  './assets/Question-27.png',
+  './assets/Question-28.png',
+  './assets/Question-29.png',
+  './assets/Question-30.png'
 ]
-const images2 = []
-const images3 = []
+const images2 = ['./assets/Question-31.png', './assets/Question-32.png', './assets/Question-33.png']
+const images3 = ['./assets/Question-61.png', './assets/Question-62.png', './assets/Question-63.png']
 const images4 = []
 
 let gabarito
@@ -60,10 +93,7 @@ btnIniciar.addEventListener('click', event => {
   }
 })
 
-const gabarito1 = ['A', 'B', 'C', 'D', 'A', 'B', 'C', 'D']
-const gabarito2 = ['D', 'B', 'C', 'C', 'D', 'A', 'B', 'C']
-const gabarito3 = ['A', 'B', 'C', 'A', 'B', 'C', 'D', 'A']
-const gabarito4 = ['A', 'B', 'C', 'D', 'D', 'B', 'B', 'C']
+
 
 let index = 0
 let tentativas = 0
@@ -134,6 +164,13 @@ options.forEach(option => {
     console.log(`Pontuação: ${pontuacao}`)
   })
 })
+
+// teclado.forEach((elem) => {
+//   elem.addEventListener('click', (e) => {
+//     getInput.value = getInput.value + e.target.innerText
+//     console.log(getInput.value)
+//   })
+// })
 
 reset.addEventListener('click', () => {
   index = 0
